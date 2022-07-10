d0160ba3b1c9b29332d2f5fa3e338e4c29cbaf89
wx <seasidesunrise@users.noreply.github.com>
2022-07-10 10:35:46 +0800

    .

--- zsl/做T策略/均线做T2_period_autobs.py
+++ zsl/做T策略/均线做T2_period_autobs.py
@@ -22,60 +22,64 @@ from bsea_utils.bsea_xyy_util import *
 策略名称 = '均线做T2'
 table_t = "bsea_做t_均线2_period"
 
+g_data = {}
+
 
 def handlebar(ContextInfo):
-    print('这是 handlebar 中的 3秒一次的tick ~~~')
+    print(f'{策略名称} 这是 handlebar 中的 3秒一次的tick ~~~')
 
     sql_all_标的 = "SELECT * FROM " + table_t + " WHERE status='1' AND account_nick='" + str(cst.account_nick) + "'"
     all_df = get_df_from_table(sql_all_标的)
     if len(all_df) == 0:
         print(f"{策略名称} {table_t} 有效标的为空，跳过")
 
+    global g_data
     for index, row in all_df.iterrows():
         qmt_code = row['qmt_code']
         name = qu.get_name_by_qmtcode(ContextInfo, qmt_code)
 
         做t均线 = get_num_by_numfield(row, '做t均线')
-        止损均线 = get_num_by_numfield(row, '做t止损均线')
+        做t止损均线 = get_num_by_numfield(row, '做t止损均线')
         ma_colname = 'ma' + str(做t均线)
         高于均线百分比卖出 = get_num_by_numfield(row, '高于均线百分比卖出')  # 如5，即表示高于均线5%卖出
-        做t资金 = get_num_by_numfield(row, '做t资金')  # 当前做t支配的资金量
+        初始做t资金 = get_num_by_numfield(row, '初始做t资金')  # 当前做t支配的资金量
         rt_当前做t状态 = get_str_by_strfield(row, 'rt_当前做t状态')
-        rt_当前做t资金 = get_num_by_numfield(row, 'rt_当前做t资金')
         period = get_str_by_strfield(row, 'period')  # 周期
         if period is None or period not in qu.period_list:
-            log_and_send_im(f"{策略名称} {table_t} {qmt_code}[{name}] period 设置错误，必须为：{qu.period_list} 其中之一，请检查，此条做T策略忽略！！")
+            log_and_send_im(f"{策略名称} {qmt_code} {name} period 设置错误，必须为：{qu.period_list} 其中之一，请检查，此条做T策略忽略！！")
             continue
         if 做t均线 <= 1:
-            log_and_send_im(f"{策略名称} {table_t} {qmt_code}[{name}]  做t均线设置错误， 做t均线：{做t均线}，请检查，此条做T策略忽略！！")
+            log_and_send_im(f"{策略名称} {qmt_code} {name} 均线设置错误， 做t均线：{做t均线}，请检查，此条做T策略忽略！！")
             continue
-        if 止损均线 <= 1:
-            log_and_send_im(f"{策略名称} {table_t} {qmt_code}[{name}] 止损均线设置错误， 止损均线：{止损均线}，请检查，此条做T策略忽略！！")
+        if 做t止损均线 <= 1:
+            log_and_send_im(f"{策略名称} {qmt_code} {name} 均线设置错误， 止损均线：{做t止损均线}，请检查，此条做T策略忽略！！")
             continue
 
-        df = get_quatation_by_params(ContextInfo, qmt_code, period, 做t均线, 止损均线)
+        df = get_quatation_by_params(ContextInfo, qmt_code, period, 做t均线, 做t止损均线)
         curr_data = df.iloc[-1]
         当前价格 = curr_data['close']
         where_clause = " WHERE qmt_code='" + qmt_code + "' AND account_nick='" + cst.account_nick + "'"
 
-        if curr_data['pre_close'] < curr_data['ma' + str(止损均线)]:  # 止损
+        if curr_data['pre_close'] < curr_data['ma' + str(做t止损均线)]:  # 止损
             # todo:  如果还有未成交的单子，是否要在57分之前先撤单
             卖出数量 = qu.get_可卖股数_by_qmtcode(qmt_code)
             if 卖出数量 == 0:
-                log_and_send_im(f"{策略名称} {table_t} {qmt_code}[{name}] 达到止损卖出条件，但卖出股数为 0，请人工检查！！")
+                key = qmt_code + "_" + get_curr_date() + "_zs"
+                if g_data.get(key) is None:
+                    g_data.update({key: '1'})
+                    log_and_send_im(f"{策略名称} {table_t} {qmt_code}[{name}] 达到止损卖出条件，但卖出股数为 0，请人工检查！！")
                 continue
-            卖出数量 = 100  # todo：仓位，测试期间暂定100股
 
+            卖出数量 = 100  # todo：仓位，测试期间暂定100股
             qu.he_sell_stock(ContextInfo, qmt_code, name, 卖出数量, 策略名称)  # 核按钮卖
 
             save_or_update_by_sql("UPDATE " + table_t + " SET status='0' " + where_clause)
-            log_and_send_im(f"{策略名称} {table_t} {qmt_code}[{name}] 达到止损卖出条件，已下单清仓！！")
+            log_and_send_im(f"{策略名称} {qmt_code} {name} 达到止损卖出条件，已下单清仓！！")
         else:
             相比均线涨幅 = curr_data['相比均线涨幅']
             if (相比均线涨幅 >= 高于均线百分比卖出 > 0) and (rt_当前做t状态 == '' or rt_当前做t状态 == '已买回'):  # 做T动作：卖出
                 持仓可卖股数 = qu.get_可卖股数_by_qmtcode(qmt_code)
-                rt_当前做t资金 = rt_当前做t资金 if rt_当前做t资金 != 0 else 做t资金
-                做t卖出股数 = int(rt_当前做t资金 / 当前价格 / 100) * 100
+                做t卖出股数 = int(初始做t资金 / 当前价格 / 100) * 100
                 卖出股数 = min(做t卖出股数, 持仓可卖股数)  # 取db中的当前持股数与持仓中的可卖股数，取数字小的那个卖出， todo：当前持股数逻辑需要讨论修改，测试期间先忽略
                 if 卖出股数 == 0:
                     print(f"{策略名称} {qmt_code}[{name}] 达到卖出条件，但卖出股数为零。做t卖出股数：{做t卖出股数}, 持仓可卖股数: {持仓可卖股数}")
@@ -84,25 +88,26 @@ def handlebar(ContextInfo):
 
                 qu.he_sell_stock(ContextInfo, qmt_code, name, 卖出股数, 策略名称)  # 核按钮卖
 
-                做t资金 += 卖出股数 * 当前价格
                 卖出后买回价与卖出价的百分比 = get_num_by_numfield(row, '卖出后买回价与卖出价的百分比')
                 rt_买回价格 = 当前价格 * 卖出后买回价与卖出价的百分比 / 100
-                t_status = T_Type.已t出.name
-                update_sql = "UPDATE " + table_t + " SET 当前做t状态='" + t_status + "', rt_当前做t资金='" + str(rt_当前做t资金) + "', rt_买回价格='" + str(rt_买回价格) + "', 当前持股数='" + str(0) + "' " + where_clause
+                t_status = T_Type.已t出.value
+                update_sql = "UPDATE " + table_t + " SET 当前做t状态='" + t_status + "', rt_买回价格='" + str(rt_买回价格) + "', 当前持股数='" + str(0) + "' " + where_clause
                 save_or_update_by_sql(update_sql)
 
             t出全部成交 = qu.check_委托是否已全部成交(qmt_code)
             if t出全部成交 and (rt_当前做t状态 == '' or rt_当前做t状态 == '已t出'):  # 做T动作：马上下单买回
                 rt_买回价格 = get_num_by_numfield(row, 'rt_买回价格')
                 if 当前价格 <= rt_买回价格:  # 价格低于买回价格，下单买回
-                    买入股数 = int(做t资金 / 当前价格 / 100) * 100
+                    买入股数 = int(初始做t资金 / 当前价格 / 100) * 100
+                    if 买入股数 < 100:
+                        print(f"{策略名称} {qmt_code}[{name}] 达到买入条件，但可买入股数不足一手。买入股数：{买入股数}, 做t资金: {初始做t资金}")
+                        continue
                     买入股数 = 100  # todo: 仓位，测试期间暂定100股
 
                     qu.he_buy_stock(ContextInfo, qmt_code, name, 买入股数, 策略名称)  # 核按钮买
 
-                    做t资金 -= 买入股数 * 当前价格
-                    t_status = T_Type.已买回.name
-                    update_sql = "UPDATE " + table_t + " SET 当前做t状态='" + t_status + "', rt_当前做t资金='" + str(rt_当前做t资金) + "', 当前持股数='" + str(买入股数) + "' " + where_clause
+                    t_status = T_Type.已买回.value
+                    update_sql = "UPDATE " + table_t + " SET 当前做t状态='" + t_status + "', 当前持股数='" + str(买入股数) + "' " + where_clause
                     save_or_update_by_sql(update_sql)
 
     d = ContextInfo.barpos
